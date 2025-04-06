5833d6ad369f112193e3e34d400b8216325f9c11
Mark Sylvester <mark@sherpa-2.lan>
2025-04-06 11:16:02 -0700

    Auto-update: 2025-04-06 11:16:02

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ['Playfair Display', 'serif'],
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        indigo: {
+          50: '#EEF2FF',
+          100: '#E0E7FF',
+          200: '#C7D2FE',
+          300: '#A5B4FC',
+          400: '#818CF8',
+          500: '#6366F1',
+          600: '#4F46E5',
+          700: '#4338CA',
+          800: '#3730A3',
+          900: '#312E81',
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
